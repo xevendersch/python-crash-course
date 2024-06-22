ccf4f07517cb8264ba949f0cc6d9a01e52824a14
xevendersch <muhammad.alqaaf_subandoko.mb5@is.naist.jp>
2024-06-22 12:20:21 +0900

    feat: modified dog.py

--- chapter-9/dog.py
+++ chapter-9/dog.py
@@ -15,7 +15,14 @@ class Dog:
         print(f"{self.name} rolled over!")
     
 my_dog = Dog('Willie', 6)
+your_dog = Dog('Lucy', 3)
 
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+# my_dog.roll_over()
+print(f"Your dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
 
